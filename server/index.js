c6614ce4c58bf32b7a5cc873c775cbaed957712a
Chetan Bisht <chetanbisht6113@gmail.com>
2026-02-10 19:39:31 +0530

    Prepare for deployment.

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ dotenv.config();
 
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 import connectDB from './config/db.js';
 
 connectDB();
@@ -18,9 +19,19 @@ import aiRoutes from './routes/aiRoutes.js';
 app.use('/api/employees', employeeRoutes);
 app.use('/api/ai', aiRoutes);
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/dist')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
